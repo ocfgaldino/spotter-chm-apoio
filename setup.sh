0bf3eb48597431ae18f8a718883f81e36d52e293
Felippe Galdino <ocfgaldino@gmail.com>
2022-02-04 00:17:07 -0300

    setup again...

--- setup.sh
+++ setup.sh
@@ -1,23 +1,20 @@
 mkdir -p ~/.streamlit/
 
-echo "
-[general]
-email = \"${HEROKU_EMAIL_ADDRESS}\"
+echo "\
+[general]\n\
+email = \"${HEROKU_EMAIL_ADDRESS}\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "
-[server]
-headless = true
-enableCORS = false
-port = $PORT
-
-[theme]
-primaryColor = "#E694FF"
-backgroundColor = "#0c0538"
-secondaryBackgroundColor = "#bac4d6"
-textColor = "#C6CDD4"
-
-
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+[theme]\n\
+primaryColor = '#E694FF'\n\
+backgroundColor = '#0c0538'\n\
+secondaryBackgroundColor = '#bac4d6'\n\
+textColor = '#C6CDD4'\n\
 " >> ~/.streamlit/config.toml
 
 
