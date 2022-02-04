ade58f19739d8e6fae6711db0af01884fe8ad0b4
Felippe Galdino <ocfgaldino@gmail.com>
2022-02-03 23:53:36 -0300

    setup file changed

--- setup.sh
+++ setup.sh
@@ -4,20 +4,16 @@ echo "\
 [general]\n\
 email = \"${HEROKU_EMAIL_ADDRESS}\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS = false\n\
 port = $PORT\n\
-
 [theme]\n\
 primaryColor = "#E694FF"\n\
 backgroundColor = "#0c0538"\n\
 secondaryBackgroundColor = "#bac4d6"\n\
 textColor = "#C6CDD4"\n\
-
-
 " > ~/.streamlit/config.toml
 
 
